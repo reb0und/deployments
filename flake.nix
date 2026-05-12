8fe30f6d133ccae369c8dfe14cac770da45a814d
reb0und <me@rebound.sh>
2026-05-12 04:11:10 -0400

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system: 
+		let 
+			pkgs = import nixpkgs {
+				inherit system;
+			};
+		in 
+		{
+			devShells.default = with pkgs; mkShell {
+				buildInputs = [
+					eza
+					ripgrep
+					fd
+					bat
+					sops
+					fluxcd
+				];
+
+				shellHook = ''
+					alias grep=rg
+					alias ls=eza
+					alias find=fd
+					alias cat=bat
+				'';
+			};
+		}
+	);
+}
