c3930cca17b829cd344b249225abf828e0350d19
reb0und <me@rebound.sh>
2026-05-12 05:03:32 -0400

    fix: bootstrap url

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 flux bootstrap git \
-	--url=ssh://git@github.com:reb0und/deployments \
+	--url=ssh://git@github.com/reb0und/deployments.git \
 	--branch=main \
+	--private-key-file=keys/flux \
 	--path=./clusters/prod \
