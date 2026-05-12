982c8265abe0b581a6a26df7c245779d94c0c2f5
reb0und <me@rebound.sh>
2026-05-12 10:10:44 -0400

    fix: cilium init script

--- scripts/cilium.sh
+++ scripts/cilium.sh
@@ -1,9 +1,11 @@
-!#/usr/bin/env sh
+#1/usr/bin/env sh
 helm repo add cilium https://helm.cilium.io/
 helm repo update
 helm install cilium cilium/cilium \
 	--namespace=kube-system \
 	--version=1.19.3 \
+	--set k8sServiceHost=172.21.0.2 \
+	--set k8sServicePort=6443 \
 	--set kubeProxyReplacement=true \
 	--set routingMode=native \
 	--set operator.replicas=1
