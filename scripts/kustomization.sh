befce75078c1ef3d8b3db60f6e0b873806b4b7ed
reb0und <me@rebound.sh>
2026-05-13 10:55:18 -0400

    feat(core): core ks

--- scripts/kustomization.sh
+++ scripts/kustomization.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
-flux create kustomization ingress \
+flux create kustomization core \
 	--source=flux-system\
-	--path="./clusters/prod/infra/ingress" \
+	--path="./clusters/prod/core/releases" \
 	--prune=true \
 	--wait=true \
 	--interval=10m \
 	--retry-interval=1m \
 	--health-check-timeout=2m \
-	--export > clusters/prod/core/kustomizations/ingress.yaml
+	--export > clusters/prod/core/kustomizations/core.yaml
