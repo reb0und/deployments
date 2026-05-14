7835bec6005d81f4d49a03207149432c24eac6f2
reb0und <me@rebound.sh>
2026-05-14 01:35:34 -0400

    feat(deployments): deployments ks

--- scripts/kustomization.sh
+++ scripts/kustomization.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
-flux create kustomization core \
+flux create kustomization deployments \
 	--source=flux-system\
-	--path="./clusters/prod/core/releases" \
+	--path="./clusters/prod/deployments" \
 	--prune=true \
 	--wait=true \
 	--interval=10m \
 	--retry-interval=1m \
 	--health-check-timeout=2m \
-	--export > clusters/prod/core/kustomizations/core.yaml
+	--export > clusters/prod/core/kustomizations/deployments.yaml
